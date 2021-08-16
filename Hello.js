59022d80a318f0db91f15480b2080a584f6b31cb
Orji Promise <O Promise@gmail.com>
2021-08-17 00:55:42 +0100

    New content

---
+++ Hello.js
@@ -0,0 +1,4 @@
+
+// const addNum = require('./add')
+console.log("hello how are you")
+// console.log(addNum(2,4))
---
+++ Klass.js
@@ -0,0 +1,6 @@
+ const  klass = (remove) => {
+   return remove.name < 2
+ }
+ let us = [{ name: 'Promise' , name: 'ayo', name: 'hammed', name: 'femi'}
+  ].filter(klass)
+console.log(us)
